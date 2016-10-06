2801e9a963349eea12dd7afc96c2b89df6298e29
kradical <konrad_here@hotmail.com>
2016-10-06 13:36:35 -0700

    changed format to adjacency matrix

--- assignment2/main.c
+++ assignment2/main.c
@@ -3,14 +3,13 @@
 #include <string.h>
 
 #define NMAX 512
-#define SENTINEL (NMAX + 1)
 
 void is_valid_vertex_count(int);
-void read_graph(int, int[NMAX][NMAX]);
-void read_vertex(int, int, int[NMAX][NMAX]);
-void check_degree(int, int);
-int is_valid_graph(int, int[NMAX][NMAX]);
-int is_valid_vertex(int, int);
+void read_graph(int, int[NMAX][NMAX], int);
+void read_vertex(int, int, int[NMAX][NMAX], int);
+void check_degree(int, int, int);
+void check_symmetric(int, int[NMAX][NMAX], int);
+void check_vertex(int, int, int, int);
 
 // Utility to check if fscanf failed to read a value.
 // Exits if fscanf(...) == 0 as this implies an invalid graph.
@@ -28,21 +27,29 @@ int main(int argc, char* argv[]) {
     }
 
     int vertex_count;
-    // adjacency list format, room for all vertices' neighbours plus 1 for writing a negative 1
+    // adjacency matrix
     int graph[NMAX][NMAX] = { 0 };
 
-    int graph_ndx = 0;
-    int invalid_node, invalid_dom_node, d;
+    int graph_ndx = 1;
     while(fscanf(stdin, "%d", &vertex_count) == 1) {
+        // TODO memset the graph to 0 between readings
+        read_graph(vertex_count, graph, graph_ndx);
+        check_symmetric(vertex_count, graph, graph_ndx);
         graph_ndx++;
-
-        read_graph(vertex_count, graph);
-        if(!is_valid_graph(vertex_count, graph)) {
-            printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
-            return EXIT_FAILURE;
-        }
     }
     return EXIT_SUCCESS;
+}
+
+// Returns the node that is invalid or 0 if they are all valid.
+// 0 will always be a valid node in a well formed graph.
+void read_graph(int vertex_count, int graph[NMAX][NMAX], int graph_ndx) {
+    is_valid_vertex_count(vertex_count);
+    printf("%5d\n", vertex_count);
+
+    int i;
+    for(i = 0; i < vertex_count; i++) {
+        read_vertex(i, vertex_count, graph, graph_ndx);
+    }
 }
 
 // Checks if the number of verties is valid.
@@ -58,41 +65,58 @@ void is_valid_vertex_count(int d) {
     }
 }
 
-// Returns the node that is invalid or 0 if they are all valid.
-// 0 will always be a valid node in a well formed graph.
-void read_graph(int vertex_count, int graph[NMAX][NMAX]) {
-    is_valid_vertex_count(vertex_count);
-    printf("%5d\n", vertex_count);
-
-    int i;
-    for(i = 0; i < vertex_count; i++) {
-        read_vertex(i, vertex_count, graph);
-    }
-}
-
 // Reads [degree] neighbours for one vertex.
-void read_vertex(int vertex, int vertex_count, int graph[NMAX][NMAX]) {
+void read_vertex(int vertex, int vertex_count, int graph[NMAX][NMAX], int graph_ndx) {
     int degree;
 
     check_int_fscanf(&degree);
-    check_degree(degree, vertex_count);
+    check_degree(degree, vertex_count, graph_ndx);
     printf("%5d ", degree);
 
-    int i;
+    int found_edges[NMAX - 1] = { 0 };
+    int i, neighbour;
     for(i = 0; i < degree; i++) {
-        check_int_fscanf(&graph[vertex][i]);
-        printf("%5d ", graph[vertex][i]);
+        check_int_fscanf(&neighbour);
+        check_vertex(vertex, neighbour, vertex_count, graph_ndx);
+        graph[vertex][neighbour] = 1;
+
+        // a multiple-edge (node has multiple edges to the same neighbouring node)
+        if(found_edges[neighbour]) {
+            printf("*** Error- graph is not simple, multiple edges between node %5d and node %5d\n", i, neighbour);
+            printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
+            exit(EXIT_FAILURE);
+        } else {
+            found_edges[neighbour] = 1;
+        }
+        printf("%5d ", neighbour);
     }
-    graph[vertex][i] = SENTINEL;
     printf("\n");
 }
 
 // Checks that the degree for a vertex is valid.
 // Exits if graph is invalid.
-void check_degree(int d, int vertex_count) {
+void check_degree(int d, int vertex_count, int graph_ndx) {
     int max = vertex_count - 1;
     if(d < 0 || d > max) {
         printf("Improper graph format. Degree %d is not in the range [0, %d]\n", d, max);
+        printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
+        exit(EXIT_FAILURE);
+    }
+}
+
+// Checks that a neighbour vertex is valid.
+// Exits if graph is invalid.
+void check_vertex(int vertex, int neighbour, int vertex_count, int graph_ndx) {
+    int max = vertex_count - 1;
+    if(neighbour < 0 || neighbour > max) {
+        printf("*** Error- invalid graph, value %5d is not in the range [0, %5d]\n", neighbour, vertex_count - 1);
+        printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
+        exit(EXIT_FAILURE);
+    }
+
+    if(vertex == neighbour) {
+        printf("*** Error- graph is not simple, loop at node %5d\n", vertex);
+        printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
         exit(EXIT_FAILURE);
     }
 }
@@ -100,52 +124,15 @@ void check_degree(int d, int vertex_count) {
 // Checks the validity of a graph (should be simple).
 // Detects self loops, not mirrored edges, and double edges
 // Returns 1 for valid and 0 for invalid.
-int is_valid_graph(int vertex_count, int graph[NMAX][NMAX]) {
-    int found_edges[NMAX - 1]; // initialized at top of for loop
-
-    int i, j, k, neighbour, found_i;
+void check_symmetric(int vertex_count, int graph[NMAX][NMAX], int graph_ndx) {
+    int i, j;
     for(i = 0; i < vertex_count; i++) {
-        memset(found_edges, 0, (NMAX - 1) * sizeof(int));
-
-        // Go through the adjacency list until sentinel
-        // (bound by vertex count to prevent access to unowned memory)
-        for(j = 0; j < vertex_count && graph[i][j] != SENTINEL; j++) {
-            // the node i's jth neighbour search through j for i
-            neighbour = graph[i][j];
-
-            // vertex value is out of range
-            if(neighbour < 0 || neighbour >= vertex_count) {
-                printf("*** Error- invalid graph, value %5d is not in the range [0, %5d]\n", neighbour, vertex_count - 1);
-                return 0;
-            }
-
-            // a loop, (node has itself in the adjacency list)
-            if(neighbour == i) {
-                printf("*** Error- graph is not simple, loop at node %5d\n", i);
-                return 0;
-            }
-
-            // a multiple-edge (node has multiple edges to the same neighbouring node)
-            if(found_edges[neighbour]) {
-                printf("*** Error- graph is not simple, multiple edges between node %5d and node %5d\n", i, neighbour);
-                return 0;
-            } else {
-                found_edges[neighbour] = 1;
-            }
-
-            found_i = 0;
-            for(k = 0; k < vertex_count && graph[neighbour][k] != SENTINEL; k++) {
-                if(graph[neighbour][k] == i) {
-                    found_i = 1;
-                    break;
-                }
-            }
-            if(!found_i) {
-                printf("*** Error- adjacency matrix is not symmetric: A[%5d][%5d] = 1, A[%5d][%5d] = 0\n",
-                    i, neighbour, neighbour, i);
-                return 0;
+        for(j = 0; j < vertex_count; j++) {
+            if(graph[i][j] != graph[j][i]) {
+                printf("*** Error- adjacency matrix is not symmetric: A[%5d][%5d] != A[%5d][%5d]\n", i, j, j, i);
+                printf("Graph   %5d: BAD GRAPH\n", graph_ndx);
+                exit(EXIT_FAILURE);
             }
         }
     }
-    return 1;
 }
