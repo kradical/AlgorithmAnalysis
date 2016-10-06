319fe597e1ae5f75f2b942b56950f09ae7fec4b2
kradical <konrad_here@hotmail.com>
2016-10-06 14:19:00 -0700

    add initial data structures

--- assignment2/main.c
+++ assignment2/main.c
@@ -10,6 +10,8 @@ void read_vertex(int, int, int[NMAX][NMAX], int);
 void check_degree(int, int, int);
 void check_symmetric(int, int[NMAX][NMAX], int);
 void check_vertex(int, int, int, int);
+void initialize_data(int*, int[NMAX], int[NMAX], int*, int[NMAX - 1], int*, int[NMAX - 1]);
+void min_dom_set(int, int, int[NMAX], int[NMAX], int, int[NMAX - 1], int, int[NMAX - 1], int[NMAX][NMAX]);
 
 // Utility to check if fscanf failed to read a value.
 // Exits if fscanf(...) == 0 as this implies an invalid graph.
@@ -26,15 +28,47 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    int vertex_count;
-    // adjacency matrix
-    int graph[NMAX][NMAX] = { 0 };
+    int vertex_count; // graph is vertex_count x vertex_count in size
+    
+    int graph[NMAX][NMAX] = { 0 }; // adjacency matrix
+    int n_dominated = 0; // number of dominated vertices
+    int num_choice[NMAX] = { 0 }; // number of times it could be dominated
+    int num_dom[NMAX] = { 0 }; // number of times it is dominated
+    int size = 0;
+    int dom[NMAX - 1] = { 0 };
+    int min_size = 0; // size of minimum dominating set so far
+    int min_dom[NMAX - 1] = { 0 }; // minimum dominating set so far
 
-    int graph_ndx = 1;
+
+    int i, j, deg_i, graph_ndx = 1;
     while(fscanf(stdin, "%d", &vertex_count) == 1) {
         // TODO memset the graph to 0 between readings
         read_graph(vertex_count, graph, graph_ndx);
         check_symmetric(vertex_count, graph, graph_ndx);
+
+        // initialize all the data
+        // n_dominated = 0;
+        
+        // memset(num_choice, 0, NMAX * sizeof(int));
+        // for(i = 0; i < vertex_count; i++) {
+        //     for(j = 0; i < vertex_count; j++) {
+        //         // will be + 1 because of the all 1 diagonal
+        //         deg_i += graph[i][j];
+        //     }
+        //     num_choice[i] = deg_i;
+        // }
+
+        // memset(num_dom, 0, NMAX * sizeof(int));
+        // size = 0;
+        // memset(dom, 0, (NMAX - 1) * sizeof(int));
+        // min_size = vertex_count;
+
+        // memset(min_dom, 0, (NMAX - 1) * sizeof(int));
+        // for(i = 0; i < vertex_count; i++) {
+        //     min_dom[i] = i;
+        // }
+
+        min_dom_set(0, n_dominated, num_choice, num_dom, size, dom, min_size, min_dom, graph);
         graph_ndx++;
     }
     return EXIT_SUCCESS;
@@ -72,6 +106,9 @@ void read_vertex(int vertex, int vertex_count, int graph[NMAX][NMAX], int graph_
     check_int_fscanf(&degree);
     check_degree(degree, vertex_count, graph_ndx);
     printf("%5d ", degree);
+
+    // a node dominates itself so we set it here
+    graph[vertex][vertex] = 1;
 
     int found_edges[NMAX - 1] = { 0 };
     int i, neighbour;
@@ -135,4 +172,14 @@ void check_symmetric(int vertex_count, int graph[NMAX][NMAX], int graph_ndx) {
             }
         }
     }
+}
+
+// recursively find the minimum dominating set
+// try to decide the status of vertex "level"
+void min_dom_set(int level, int n_dom, int num_choice[NMAX], int num_dom[NMAX], int size,
+    int dom[NMAX - 1], int min_size, int min_dom[NMAX - 1], int graph[NMAX][NMAX]) {
+    // make vertex level blue
+    // make vertex level red
+    // change back to white before returning
+    printf("%d\n", level);
 }
