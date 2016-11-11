7fd715317d46ae6e7b3d0fdf489e90cbd2f37c5a
kradical <konrad_here@hotmail.com>
2016-11-11 13:14:19 -0800

    random heuristic finished

--- project/random/main.c
+++ project/random/main.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/times.h>
 
-// This program calculates the minimum dominating set for a series of graphs.
+// This program calculates a minimal dominating set for a graph given a heuristic and a time limit
+// The heuristic for this program is random.
 // Graphs are provided through standard input in the format specified by assignment 2.
 // The maximum size of a graph may be altered using NMAX and then recompiling.
-// The DEBUG flag may be set to 1 for verbose debugging output.
-
-#define DEBUG 0
-#define NMAX 128
+#define NMAX 2187
 #define MMAX ((NMAX +31)/ 32)
 
-/* set operations (setadd is its address, pos is the bit number): */
-#define SETWD(pos) ((pos)>>5)    /* number of longword containing bit pos */
-#define SETBT(pos) ((pos)&037)   /* position within longword of bit pos */
+// This bitset operations code was taken from Brendan McKay's nauty
+/*****************************************************************************
+*                                                                            *
+*   Conventions and Assumptions:                                             *
+*                                                                            *
+*       A 'longword' is the chunk of memory that is occupied by a long int   *
+*       We assume that longwords have at least 32 bits.
+*       The rightmost 32 bits of longwords are numbered 0..31, left to       *
+*       right.                                                               *
+*                                                                            *
+*       The int variables m and n have consistent meanings througout.        *
+*       Graphs have n vertices always, and sets have m longwords.            *
+*                                                                            *
+*       A 'set' consists of m contiguous longwords, whose bits are numbered  *
+*       0,1,2,... from left (high-order) to right (low-order), using only    *
+*       the rightmost 32 bits of each longword.  It is used to represent a   *
+*       subset of {0,1,...,n-1} in the usual way - bit number x is 1 iff x   *
+*       is in the subset.  Bits numbered n or greater, and unnumbered bits,  *
+*       are assumed permanently zero.                                        *
+*                                                                            *
+*****************************************************************************/
+// set operations (setadd is its address, pos is the bit number):
+#define SETWD(pos) ((pos)>>5)    // number of longword containing bit pos
+#define SETBT(pos) ((pos)&037)   // position within longword of bit pos
 #define ADD_ELEMENT(setadd,pos) ((setadd)[SETWD(pos)] |= bit[SETBT(pos)])
 #define DEL_ELEMENT(setadd,pos) ((setadd)[SETWD(pos)] &= ~bit[SETBT(pos)])
 #define IS_ELEMENT(setadd,pos) ((setadd)[SETWD(pos)] & bit[SETBT(pos)])
 
-/* number of 1-bits in longword x */
+// number of 1-bits in longword x
 #define POP_COUNT(x) bytecount[(x)>>24 & 0377] + bytecount[(x)>>16 & 0377] \
                         + bytecount[(x)>>8 & 0377] + bytecount[(x) & 0377]
 
@@ -48,6 +69,8 @@ int bit[] = {
     010000,04000,02000,01000,0400,0200,0100,040,020,010,04,02,01
 };
 
+float start_timer();
+float check_timer();
 void check_int_scanf(int*, int);
 int read_graph(int*, int*, int[NMAX][MMAX], int);
 void check_vertex_count(int, int);
@@ -57,13 +80,30 @@ void check_vertex(int, int, int);
 void check_graph(int, int[NMAX][MMAX], int);
 void print_graph(int, int[NMAX][MMAX]);
 void initialize_p(int, int[NMAX]);
+int set_size(int, int*);
 void print_set(int, int*);
 int find_dom_set(int, int*, int[NMAX], int[NMAX], int*, int[MMAX], int, int, int[NMAX][MMAX], int[NMAX]);
 void print_dom_set(int, int, int*);
 
-#if DEBUG
-    void print_vec(int, int*);
-#endif
+// Verbose output flag
+int verbose;
+
+// Timing code from Wendy Myrvold's example timing code
+static struct tms start_time;
+int max_second;
+
+// Get the start time
+float start_timer() {
+    times(&start_time);
+}
+
+// Get the time elapsed since start time
+float check_timer() {
+    struct tms end;
+    times(&end);
+
+    return (float) (end.tms_utime - start_time.tms_utime) / 60;
+}
 
 // Utility to check if scanf failed to read a value.
 // Parameters:
@@ -80,13 +120,16 @@ void check_int_scanf(int* d, int graph_num) {
 }
 
 int main(int argc, char* argv[]) {
-    if(argc != 1) {
-        printf("USAGE:\t%s", argv[0]);
-        return EXIT_FAILURE;
+    if(argc!= 3) {
+         printf("Usage %s <maximum number of seconds per graph> <verbose>\n", argv[0]);
+         return EXIT_FAILURE;
     }
+    max_second = atoi(argv[1]);
+    verbose = atoi(argv[2]);
+
 
     int vertex_count; // graph is vertex_count x vertex_count in size
-    int m;
+    int m; // size of compressed adjcency matrix
     int G[NMAX][MMAX]; // compressed adjacency matrix form of a graph
     int n_dominated; // number of dominated vertices
     int num_choice[NMAX]; // number of times it could be dominated
@@ -95,15 +138,34 @@ int main(int argc, char* argv[]) {
     int dom[MMAX]; // current dominating set
     int p[NMAX];
 
+    int min_dom[MMAX]; // minimum dominating set
+    int min_size; // size of minimum dominating set found so far
+
     int graph_num = 1;
     int i, j, deg_i, max_deg;
     while(read_graph(&vertex_count, &m, G, graph_num)) {
         check_graph(vertex_count, G, graph_num);
-        print_graph(vertex_count, G);
 
-        initialize_p(vertex_count, p);
-        find_dom_set(0, &n_dominated, num_choice, num_dom, &size, dom, vertex_count, max_deg, G, p);
-        print_dom_set(size, vertex_count, dom);
+        min_size = vertex_count;
+        memset(min_dom, 0, MMAX * sizeof(int));
+        do {
+            initialize_p(vertex_count, p);
+            find_dom_set(0, &n_dominated, num_choice, num_dom, &size, dom, vertex_count, max_deg, G, p);
+            
+            if(size < min_size) {
+                min_size = size;
+                memcpy(min_dom, dom, MMAX * sizeof(int));
+            }
+        } while(check_timer() < (float) max_second);
+
+        // Print the results.
+        if(verbose) {
+           print_graph(vertex_count, G);
+           print_dom_set(min_size, vertex_count, min_dom);
+        } else {
+           printf("%4d %4d   %3d\n", graph_num, vertex_count, min_size);
+        }
+        fflush(stdout);
 
         graph_num++;
     }
@@ -263,7 +325,7 @@ void print_graph(int vertex_count, int G[NMAX][MMAX]) {
     }
 }
 
-// Randomize method taken from wendy myrvold's 425 slides.
+// Randomize method taken from Wendy Myrvold's example code.
 void initialize_p(int vertex_count, int p[NMAX]) {
     int i;
     for(i = 0; i < vertex_count; i++) {
@@ -281,6 +343,7 @@ void initialize_p(int vertex_count, int p[NMAX]) {
 }
 
 // Compute the size of a set.
+// Taken from Wendy Myrvold's example code.
 int set_size(int n, int set[]) {
     int j, m, d;
 
@@ -295,6 +358,7 @@ int set_size(int n, int set[]) {
 }
 
 // Prints a set.
+// Taken from Wendy Myrvold's example code.
 void print_set(int n, int set[]) {
    int i;
 
@@ -322,15 +386,6 @@ void print_set(int n, int set[]) {
 // The colors refer to vertex states outlined in the slides.
 int find_dom_set(int level, int* n_dom, int num_choice[NMAX], int num_dom[NMAX], int* size,
     int dom[MMAX], int vertex_count, int max_deg, int G[NMAX][MMAX], int p[NMAX]) {
-    
-    #if DEBUG
-        printf("\nLEVEL: %d\n", level);
-        printf("# vertices dominated: %d\n", *n_dom);
-        printf("# choices per vertex:\n");
-        print_vec(vertex_count, num_choice);
-        printf("# times dominated:\n");
-        print_vec(vertex_count, num_dom);
-    #endif
 
     // initialize all the data
     if(level == 0) {
@@ -430,16 +485,3 @@ void print_dom_set(int size, int vertex_count, int* dom) {
     print_set(vertex_count, dom);
     printf("\n");
 }
-
-#if DEBUG
-// Utility for printing arrays.
-// Parameters:
-//   size: the size of the array to print.
-//   vec: the array to be printed.
-void print_vec(int size, int* vec) {
-    int i;
-    for(i = 0; i < size; i++) {
-        printf("%d: %d\n", i, vec[i]);
-    }
-}
-#endif
